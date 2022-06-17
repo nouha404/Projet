226a29ec7280f31711c762d7b1546894472c7d5d
nouha404 <97695765+nouha404@users.noreply.github.com>
2022-06-17 00:15:22 +0000

    Add files via upload

--- exercice/main.py
+++ exercice/main.py
@@ -1,35 +1,41 @@
+# - Créer une classe Joueur
+# - on aura 3 attributs (pseudo, health(points de vie), damage(dégats par tir))
+# - Créer une méthode getDamage qui enlève des points de vie selon
+# les dégats infligés par un autre joueur.
+# - Créer une méthode shoot qui permet
+# d'infliger des dégats à un autre joueur
+
 from dataclasses import dataclass
 from typing import ClassVar
 
 @dataclass
-class Voiture:
-    marque : str
-    vitesse : int
-    prix : str
-    
-    #STR s'execute directement apres les attribut d'instances
-    def __str__(self):
-        return f"Voiture de marque {self.marque} avec une vitesse de {self.vitesse}"
-    
-    @classmethod
-    def lamborghini(cls):
-        return cls(marque='Lamborghini', vitesse=400, prix='5M')
-    
-    def __post_init__(self):
-        return f"Voiture de marque {self.marque} avec une vitesse de {self.vitesse}"
-    
-    @classmethod
-    def porsh(cls):
-        return cls(marque='Porsh', vitesse=390, prix='4.5M')
+class Joueur:
+    pseudo : str
+    damage : int = 5
+    health : int = 100
     
     
+    def getDamage(self, degat_receive, pseudo_adversaire):
+        self.health -= degat_receive
+        
+        print(f"{pseudo_adversaire} vous a infligé {degat_receive} de dommage")
+    
+    def shoot(self, adversaire):
+        adversaire.getDamage(self.damage, self.pseudo) 
+        
 
 
-lambo = Voiture.lamborghini()
-porsh = Voiture.porsh()
-
-test = Voiture('car rapide', 450, '150k')
-print(lambo.marque)
+nouha = Joueur('nouha', 10)
+zlorg = Joueur('zlorg', 15)
 
 
-# print(lambo.prix)
+zlorg.shoot(nouha)
+
+nouha.shoot(zlorg)
+# print(zlorg.health)
+
+
+
+                
+    
+    
